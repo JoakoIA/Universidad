2bd19e4fc0589f598ae3fa60d12186c69dda643e
JoakoIA <135570223+JoakoIA@users.noreply.github.com>
2023-10-17 14:10:49 -0300

    b

---
+++ Python/Taller IV/Marcov_chain.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pymc3 as pm
+import matplotlib.pyplot as plt
+#import arbiz as az
+
+data = np.array([1,1,1,1,0,1,0,0,1,1])
+
+with pm.Model() as coin_model:
+    p = pm.Uniform("p", 0, 1)
+    likelihood = pm.Bernoulli("likelihood", p, observed=data)
+    trace = pm.sample(1000, tune = 1000)
+
+pm.summary(trace)
+az.plot_trace(trace, combined = True)
---
+++ R/Taller IV.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(readr)
+
+data <- read_rds('data_agvAPI.rds')
+info <- read_rds('metadata_estaciones_agvAPI.rds')
