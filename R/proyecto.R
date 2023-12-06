eb527e6c4c3806563bade76c7b08c6a106373342
JoakoIA <135570223+JoakoIA@users.noreply.github.com>
2023-12-05 22:35:24 -0300

    sa

--- R/proyecto.R
+++ R/proyecto.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 data_era5 <- read_rds("R/datos_temp_media_ERA5Land_estaciones_agromet.rds")
 
-data_agromet <- read_rds("R/data_agromet_proyecto_final_2015-2022.rds")
+   <- read_rds("R/data_agromet_proyecto_final_2015-2022.rds")
 
 #Cambiar los numeros de las estaciones por los codigos emas
 emas <- estaciones_agromet %>% 
@@ -11,36 +11,33 @@ emas <- estaciones_agromet %>%
 
 names(data_era5)[2:418] <- emas
 
-data_era5 %>% 
-  glimpse()
-
-#Modificar la fecha de tipo caracter a tipo fecha
-data_era5 %>% 
-  mutate(date = ymd(date)) %>% 
-  filter(between(date,ymd("2015-01-01"),ymd("2022-12-31")))
-  select(all_of(cod_emas))
-
+#Seleccionar las columnas de interes
 cod_emas <- estaciones_agromet %>%
-  filter(region == "Biobío") %>%0
+  filter(region == "Biobío") %>%
   pull(ema)
 
-cod_emas <- cod_emas[1:30]
+cod_emas <- as.character(cod_emas)
 
-data_era5_a <- data_era5 %>% 
-  mutate(date = ymd(date)) %>% 
-  filter(between(date,ymd("2015-01-01"),ymd("2022-12-31"))) %>%
-  select(all_of(cod_emas))
-#Seleccionar valores unicos de la columna region
-estaciones_agromet %>% 
-  select(region) %>% 
-  unique()
+data_era5_Biobio <- data_era5 |> 
+  dplyr::mutate(date = lubridate::ymd(date)) |> 
+  dplyr::filter(dplyr::between(date, ymd("2015-01-01"), ymd("2022-12-31")))  |> 
+  dplyr::select(date, all_of(cod_emas))
 
-#Datos agromet
-data_agromet %>% 
-  glimpse()
+readr::write_rds(data_era5_Biobio,'R/data_era5_arica_2015-2022.rds')
 
-#Seleccionar las columnas de interes
-data_agromet %>%
-  select(station_id:temp_promedio_aire) %>%
-  filter(between(fecha_hora, ymd("2015-01-01"), ymd("2022-12-31"))) %>%
-  filter(station_id %in% cod_emas)
+data_agro_sel <- data_agromet |> 
+  dplyr::select(station_id:temp_promedio_aire) |> 
+  dplyr::filter(dplyr::between(fecha_hora,ymd("2015-01-01"),ymd("2022-12-01"))) |> 
+  dplyr::filter(station_id %in% as.numeric(cod_emas)) 
+
+data_agro_sel |> 
+  dplyr::group_by(station_id,
+                  mes = lubridate::floor_date(fecha_hora,'month')) |> 
+  dplyr::summarize(NAs = sum(is.na(temp_promedio_aire)),.groups = 'drop') |> 
+  mutate(station_id = as.factor(station_id),
+         mes_plot = format(mes, "%y-%b")) |> 
+  ggplot(aes(mes_plot,station_id,fill=NAs)) +
+  geom_tile() +
+  scale_fill_viridis_c() +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90))
